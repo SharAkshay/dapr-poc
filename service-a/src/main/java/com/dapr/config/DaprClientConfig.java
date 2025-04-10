62fa8bec544f63e49aa8e8e2bc49bebb94f99806
Akshay Sharma <c-akshay.sharma@crypto-finance.com>
2025-04-10 10:31:42 +0300

    added resiliency options

--- service-a/src/main/java/com/dapr/config/DaprClientConfig.java
+++ service-a/src/main/java/com/dapr/config/DaprClientConfig.java
@@ -2,8 +2,11 @@ package com.dapr.config;
 
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.resiliency.ResiliencyOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 /**
  * Configuration class for creating a DaprClient bean.
@@ -18,6 +21,12 @@ public class DaprClientConfig {
      */
     @Bean
     public DaprClient daprClient() {
-        return new DaprClientBuilder().build();
+        ResiliencyOptions resiliencyOptions = new ResiliencyOptions()
+                .setMaxRetries(3)
+                .setTimeout(Duration.ofSeconds(5));
+
+        return new DaprClientBuilder()
+                .withResiliencyOptions(resiliencyOptions)
+                .build();
     }
 }
