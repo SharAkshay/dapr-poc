739d2e42a8bae0a87ca2d64075f0916f7e5abcc5
Akshay Sharma <c-akshay.sharma@crypto-finance.com>
2025-04-10 10:33:55 +0300

    removed unwanted code

--- service-b/src/main/java/com/dapr/controller/PongController.java
+++ service-b/src/main/java/com/dapr/controller/PongController.java
@@ -5,7 +5,6 @@ import com.dapr.service.PongService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
